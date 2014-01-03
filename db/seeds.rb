2d465590c53c54f97a33caedc8276c41f433f36f
neil patel <neil@myindianweddingapp.com>
2014-01-03 18:21:40 +0000

    updated seeders.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 #   first quote
 post1 = Post.new
-post1.title "Jacques Yves Cousteau"
+post1.title = "Jacques Yves Cousteau"
 post1.kind = "text"
 post1.content = "Mankind has probably done more damage to the Earth in the 20th century than in all of previous human history."
 post1.save
@@ -19,7 +19,7 @@ end
 
 # 2nd quote
 post2 = Post.new
-post2.title "Martin Scorsese"
+post2.title = "Martin Scorsese"
 post2.kind = "text"
 post2.content = "Film in the 20th century, it's the American art form, like jazz.."
 post2.save
@@ -27,18 +27,18 @@ post2.save
 5.times { post2.hearts.create }
 
 # 3nd quote with image
-post3 = Post.create{
-title: "Julie Andrews,"
-kind: "image"
-content: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQRj9JRo-JiTARUpwtssGoEdM-wVeufPDpuUiQUdx4e1Mc7kHdMQ.jpg"
-}
+post3 = Post.new
+post3.title = "Julie Andrews"
+post3.kind = "image"
+post3.content = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQRj9JRo-JiTARUpwtssGoEdM-wVeufPDpuUiQUdx4e1Mc7kHdMQ.jpg"
+post3.save 
 
 
 8.times { post3.hearts.create }
 
 # 4th quote with image
 post4 = Post.new
-post4.title "Salvador Dali"
+post4.title = "Salvador Dali"
 post4.kind = "image"
 post4.content = "http://twistedsifter.files.wordpress.com/2012/03/salvador-dali-famous-quote-perfection-art-creativity1.jpg"
 post4.save
